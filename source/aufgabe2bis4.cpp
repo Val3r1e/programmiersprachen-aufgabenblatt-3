60724b53f5952c6be35f4ba126e4e479530ecfcf
Valerie Lemuth <valerie.lemuth@uni-weimar.de>
2017-05-15 15:39:08 +0200

    Up to 3.3

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -1,19 +1,36 @@
 #include <list>
 #include <vector>
+#include <set>
 # include <cstdlib>
 # include <iostream> // std :: cout
+# include <iterator> // std :: ostream_iterator <>
+# include <algorithm> // std :: reverse , std :: generate
+
 
 int main (){
-    std::list <unsigned int> l1 (100); //int kann nicht neg. werden
+    std::list <unsigned int> l1 (100);           //int kann nicht neg. werden
     for (auto& l : l1) {
-		l = std::rand() % 101;              //Fügt von 0-100 random zahlen ein
-	}
-    /*std::copy(std::begin(l1), std::end(l1), 
-		std::ostream_iterator <int>(std::cout, "\n"));*/
-    
+		l = std::rand() % 101;                       //Fügt von 0-100 random zahlen ein
+	  }
+
     std::vector<unsigned int > v1 (l1.size());
     std::copy(std::begin(l1), std::end(l1), std::begin(v1));
-    
-    /*std::copy(std::begin(l1), std::end(l1), 
-		std::ostream_iterator <int>(std::cout, "\n"));*/
+
+    std::set <unsigned int> s1;
+
+    for(auto& s : l1){                            //Jede Zahl kommt nur einmal vor 
+      s1.insert(s);
+    }
+
+    std::cout<< "Es befinden sich " << s1.size() << " verschiedene Zahlen in der Liste. \n";
+    std::cout<< "Insgesamt befinden sich " << l1.size() << " Zahlen in der Liste. \n";
+
+    std::cout << "Die Liste enthält nicht: \n";
+    for (int i = 0; i <= 100; ++i){
+		  if(s1.find(i) == s1.end()){
+			  std::cout << i << "\n";
+		  }
+	  }
+
+
 }
