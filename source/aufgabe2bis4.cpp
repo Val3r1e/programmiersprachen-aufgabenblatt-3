210b2d8d74e3354e56107f3a7fb0eaa7a03763f1
Valerie Lemuth <valerie.lemuth@uni-weimar.de>
2017-05-21 08:50:50 +0200

    Minor changes -done

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -9,16 +9,17 @@
 
 
 int main (){
+    //Aufg. 3.2
+
     std::list <unsigned int> l1 (100);           //int kann nicht neg. werden
     for (auto& l : l1) {
 		  l = std::rand() % 101;                       //Fügt von 0-100 random zahlen ein
 	  }
 
-    /*std::copy(std::begin(l1), std::end(l1), 
-		std::ostream_iterator <int>(std::cout, "\n"));*/
-
     std::vector<unsigned int > v1 (l1.size());                //Kopieren list in vector
     std::copy(std::begin(l1), std::end(l1), std::begin(v1));
+
+    //Aufg. 3.3
 
     std::set <unsigned int> s1;
 
@@ -42,20 +43,22 @@ int main (){
     std::copy(std::begin(s2), std::end(s2), 
 		std::ostream_iterator <int>(std::cout, "\n"));
 
+    //Aufg. 3.4
 
     std::map <int,int> m1;          // <Zahl,Häufigkeit>
-    int j = 1;
 
     for (auto i = v1.begin(); i != v1.end(); ++i){      // map füllen mit Zahl(Key) und value um 1 erhöhen 
       m1.insert(std::make_pair(*i , 0));
-      ++m1[*i];
+      ++m1[*i];                                         //erhöht value nochmal wenn key doppelt vorkommt
     }
 
     std::cout << "Zahlen und ihre Häufigkeit: \n";
 
     for(auto it = m1.begin(); it != m1.end(); ++it){      //map printen
-      std::cout << it->first << ":" << it ->second << "\n";
+      std::cout << it->first << ":" << it->second << "\n";
     }
 
+    //map bietet sich an weil der Key(Zahl) unveränderlich ist und nur einmal vorkommt aber die Value(Anzahl) verändert werden kann 
+    //Also wenn Key mehr als einmal vorkommt 
 
 }
--- source/beispiel.cpp
+++ source/beispiel.cpp
@@ -7,27 +7,27 @@
 
 int main ()
 {
-	std::vector <int> v0(10);
-	for (auto& v : v0) {
+	std::vector <int> v0(10);											//vector v0 mit 10 Elementen
+	for (auto& v : v0) {												//for-Schleife füllt v0 mit random Nummern
 		v = std::rand();
 	}
-	std::copy(std::begin(v0), std::end(v0), 
+	std::copy(std::begin(v0), std::end(v0), 							//Kopiert von begin(v0) bis end(v0) in ostream_iterator --> gibt v0 aus 
 		std::ostream_iterator <int>(std::cout, "\n"));
 
-	std::list <int> l0(v0.size());
-	std::copy(std::begin(v0), std::end(v0), std::begin(l0 ));
+	std::list <int> l0(v0.size());										//Neue list l0 mit größe von v0 (10)
+	std::copy(std::begin(v0), std::end(v0), std::begin(l0));			//Kopiert von begin(v0) bis end(v0) alle elemente in l0, von begin(l0) an 
 
-	std::list <int> l1(std::begin(l0), std::end(l0));
-	std::reverse(std::begin(l1), std::end(l1));
-	std::copy(std::begin(l1), std::end(l1), 
+	std::list <int> l1(std::begin(l0), std::end(l0));					//Erstellt l1 aus l0
+	std::reverse(std::begin(l1), std::end(l1));							//dreht l1 um
+	std::copy(std::begin(l1), std::end(l1), 							//Gibt l1 aus
 		std::ostream_iterator <int>(std::cout, "\n"));
 
-	l1.sort ();
-	std::copy(l1.begin(), l1.end(), 
+	l1.sort ();															//Sortiert l1 nach größe
+	std::copy(l1.begin(), l1.end(), 									//Gibt l1 aus
 		std::ostream_iterator <int>(std::cout, "\n"));
 
-	std::generate(std::begin(v0), std::end(v0), std::rand );
-	std::copy(v0.rbegin(), v0.rend (), 
+	std::generate(std::begin(v0), std::end(v0), std::rand);				//Füllt v0 mit neuen Zufallszahlen
+	std::copy(v0.rbegin(), v0.rend(), 									//Gibt v0 aus
 		std::ostream_iterator <int>(std::cout, "\n"));
 
 	return 0;
